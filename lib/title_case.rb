e1279d6fd0f122045384296ea17c3f81d5b4da6e
Kendra Ash & Yelena Nedelko <student@epicodus.com>
2015-08-03 16:51:48 -0700

    Add Spec 6: Handels all capital letters in a String

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,16 +1,13 @@
 class String
   define_method(:title_case) do
     small_words = ["a", "an", "and", "the", "but", "or", "for", "nor","to","from"]
-    split_sentence=self.split()
+    split_sentence=self.downcase.split()
     split_sentence[0].capitalize!()
     split_sentence.each() do |word|
     if (small_words).include?(word).!() || ("a".."z").cover?(word).!()
       word.capitalize!()
     end
-    if ("A".."Z").cover?(split_sentence).!() && split_sentence[0].!()
-        word.downcase()
-    end
-    end
+  end
     split_sentence.join(" ")
   end
 end
